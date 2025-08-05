3c1751f5df960bca7c1ac70f448451d71c0b36d3
valpryz <valpryz@gmail.com>
2025-08-05 13:37:14 +0100

    exercise 3.2 step 2 completed

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ const persons = [
     }
 ]
 
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+})
+
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
