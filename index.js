3a2cb4f2e51bec2cb24bae0a5c8b3ea5d214ae98
valpryz <valpryz@gmail.com>
2025-08-05 13:59:21 +0100

    exercise 3.4 step 4 completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -41,6 +41,12 @@ app.get('/api/persons/:id', (req, res) => {
   }else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
 })
 
 
