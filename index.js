0831151c2a05122101e2fe57825c0c13951d3793
valpryz <valpryz@gmail.com>
2025-08-07 10:54:58 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('person', function (req, res) { 
   return JSON.stringify(req.body)
