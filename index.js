0b9cd081e3052ecce3de02ada91edc616410d061
valpryz <valpryz@gmail.com>
2025-08-13 18:28:56 +0100

    review the deletion

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/api/persons/:id', (req, res, next) => {
 
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
-    .then(res => res.status(204).end())
+    .then(() => res.status(204).end())
     .catch(error => next(error))
 })
 
